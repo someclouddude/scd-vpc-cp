e04094acf6bc39222621954ca5255b91186d79fd
Ian Evans <someclouddude@gmail.com>
2025-08-10 08:48:19 -0500

    fix: use nat_instance_id for NAT instance in private route table

--- tofu/routes.tf
+++ tofu/routes.tf
@@ -18,8 +18,8 @@ resource "aws_route_table" "private" {
   count  = var.az_count
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block     = "0.0.0.0/0"
-    instance_id    = aws_instance.nat[count.index].id
+    cidr_block      = "0.0.0.0/0"
+    nat_instance_id = aws_instance.nat[count.index].id
   }
   tags = { Name = "private-rt-${count.index + 1}" }
 }
